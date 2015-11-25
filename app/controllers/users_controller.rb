e1cbff0e7f0c608acea0ea987a7307c762ed0cff
Taka <o.f.d.m.d.s.6666@gmail.com>
2015-11-25 17:44:55 +0000

    authenticate added to users and relation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!
   def friends
+  	own_user = User.find(params[:id])
+  	unless own_user == current_user
+  		redirect_to root_path, alert: t("can't show other's friends list")
+  	end
   	@friends = User.find(params[:id]).friends
   	@user = User.new
   end
   def search
   	word = params[:word]
   	@search_result = User.where('username like ? AND id != ?', "%#{word}%", current_user.id)
-  	# render json: @search_result
   end
 end

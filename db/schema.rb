a597e9c2d3f6b25dcf6e753dc560cdf89dc8deb0
Taka <o.f.d.m.d.s.6666@gmail.com>
2015-12-06 15:59:40 +0000

    rails g migration AddConfirmationToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123194429) do
+ActiveRecord::Schema.define(version: 20151206153935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
